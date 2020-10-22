e8f5a2a9f2c1935f08cf51f835e0082d03fed391
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-10-22 13:13:38 -0400

    Add a test for ArrayOps.largest()

--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester {
     public static void main(String[] args) {
         System.out.println(ArrayOps.sum(new int[]{3, 4, 5}));
+        System.out.println(ArrayOps.largest(new int[]{3, 4, 5}));
     }
 }
